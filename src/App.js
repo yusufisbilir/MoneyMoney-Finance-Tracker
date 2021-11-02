00f425722c76a08ff86f5a2441ca95667ea38904
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-02 16:17:48 +0300

    Route pages with react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//pages and components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
