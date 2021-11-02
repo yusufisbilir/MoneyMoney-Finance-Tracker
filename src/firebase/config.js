4c6b3f4baf9d9d17c98a8d6f2b6d00015ece30b1
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-02 22:19:43 +0300

    Config firebase auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-LCy4GxI6Ihi9m4yqzqCjAefnxYXxAkc",
@@ -10,8 +11,11 @@ const firebaseConfig = {
   appId: "1:183300837498:web:7a16e925baae0412acebc3",
 };
 
+//init
 firebase.initializeApp(firebaseConfig);
 
+//services
 const projectFireStore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-export { projectFireStore };
+export { projectFireStore, projectAuth };
