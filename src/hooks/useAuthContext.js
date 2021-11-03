72038a2198cc0b5be7cad992afb680d5d3825e83
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-03 13:12:21 +0300

    Create useAuthContext custom hook.

---
+++ src/hooks/useAuthContext.js
@@ -0,0 +1,12 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw Error("useAuthContext must be inside an AuthContextProvider");
+  }
+
+  return context;
+};
