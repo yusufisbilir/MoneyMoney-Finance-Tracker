32b44cbb126eba4f9bfc943159e93dc869fffb9d
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-04 17:33:36 +0300

    Create useLogout custom hook.

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      await projectAuth.signOut();
+      dispatch({ type: "LOGOUT" });
+
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+      setIsPending(false);
+    }
+  };
+
+  return { logout, error, isPending };
+};
