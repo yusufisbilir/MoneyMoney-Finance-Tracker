2657c0d18735f0e5851f23d7363a02178c0f75c6
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-05 20:43:34 +0300

    Firebase auth satate changes.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext();
 
@@ -8,13 +9,25 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOGOUT":
       return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
 };
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+  });
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
+    });
+  }, []);
 
   console.log("Authcontext satate:", state);
 
