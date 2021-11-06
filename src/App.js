0a6c1c5611c85418c0adc6b4f0dae435197ad82a
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-06 19:55:45 +0300

    Route guarding.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 //pages and components
@@ -8,7 +13,7 @@ import Signup from "./pages/signup/Signup";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
       {authIsReady && (
@@ -16,13 +21,16 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              <Home />
+              {!user && <Redirect to="/login" />}
+              {user && <Home />}
             </Route>
             <Route path="/login">
-              <Login />
+              {user && <Redirect to="/" />}
+              {!user && <Login />}
             </Route>
             <Route path="/signup">
-              <Signup />
+              {user && <Redirect to="/" />}
+              {!user && <Signup />}
             </Route>
           </Switch>
         </Router>
