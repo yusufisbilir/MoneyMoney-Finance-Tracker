ae5cece091d4fa9b100a3a8ffbf09a11cc759d4c
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-07 19:56:44 +0300

    Create useFirestore custom hook.

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,75 @@
+import { useReducer, useEffect, useState } from "react";
+import { projectFireStore } from "../firebase/config";
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null,
+};
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case "IS_PENDING":
+      return {
+        isPending: true,
+        document: null,
+        success: false,
+        error: null,
+      };
+    case ADDED_DOCUMENT:
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  //collection ref
+  const ref = projectFireStore.collection(collection);
+
+  //only dispatch is not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
+  // add document
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const addedDocument = await ref.add(doc);
+      dispatchIfNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (error) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: error.message });
+    }
+  };
+
+  const deleteDocument = (id) => {};
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, deleteDocument, response };
+};
