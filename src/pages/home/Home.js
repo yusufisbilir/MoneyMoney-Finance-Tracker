067b841d0fcd0d1e50595915e36fa5cad35f303c
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-07 19:21:56 +0300

    Create transaction form.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,16 @@
 import styles from "./Home.module.css";
 import React from "react";
+import TransactionForm from "./TransactionForm";
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>transaction list</h1>
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   );
 }
---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function TransactionForm() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ name, amount });
+  };
+  return (
+    <>
+      <h3>Add Transaction</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Transaction name: </span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Amount (₺): </span>
+          <input
+            type="number"
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+          />
+        </label>
+
+        <button>Add Transaction</button>
+      </form>
+    </>
+  );
+}
